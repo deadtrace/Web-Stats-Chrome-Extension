8a6c64e1e755b2b010011eff2e47525b729e1cb9
Stanislav K <justtrace17@gmail.com>
2019-03-16 00:50:33 +0300

    Added WindowFocusChanges Tracker and getActibeURLfromTabs function

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 console.log("background.js is loaded"); 
 
 function time() { 
-return (new Date()).getTime(); 
+    return (new Date()).getTime(); 
 } 
 
 lastTime = time();
@@ -11,8 +11,33 @@ function shrinkURL(url) {
     return (new URL(url)).hostname;
 }
 
+// function getActiveURL(callback){
+//     chrome.tabs.query({
+//         active: true,
+//         currentWindow: true
+//     }, function(tabs) {
+//         callback(shrinkURL(tabs[0].url));
+//     })
+//     console.log("where am i");
+// }
+
+function getActiveURLfromTabs(tabs) {
+    for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].active) {
+            return shrinkURL(tabs[i].url);
+        }
+    }
+}
+
+chrome.tabs.query({active: true}, function(tabs) {
+    var t = tabs[0].url;
+    currentURL = shrinkURL(t);
+})
+
 function changePage(tab) {
-    console.log("you were on", currentURL, time() - lastTime, "ms");
+    if (currentURL != "") {
+		console.log("you were on", currentURL, time() - lastTime, "ms");
+	}
     lastTime = time();
     currentURL = shrinkURL(tab.url);
 }
@@ -40,3 +65,28 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
         changePage(tabs[0])
     })
 });
+
+
+chrome.windows.onFocusChanged.addListener(function() {
+    console.log("");
+    chrome.windows.getCurrent({populate: true}, function(window) {
+        counter++;
+        console.log(counter, window.tabs);
+        
+        console.log(getActiveURLfromTabs(window.tabs));
+
+        // getActiveURL(function(a, b) {
+        //     console.log(a, b);
+        // })
+
+
+
+    });
+});
+/*
+window.onblur = function() {
+	counter++;
+    console.log("");
+    console.log("window is blured");
+}
+*/
